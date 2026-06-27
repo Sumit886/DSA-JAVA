ec5d8cc38a910cda250786daed254469581a4285
Sumit Kumar <sk1019405@gmail.com>
2026-06-27 09:13:54 +0530

    Day 1 -Leetcode 121

---
+++ src/Arrays/BestTimeToBuySellStock.java
@@ -0,0 +1,27 @@
+package Arrays;
+
+public class BestTimeToBuySellStock {
+
+    public static int maxProfit(int[] prices) {
+
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+
+            int profit = prices[i] - minPrice;
+            maxProfit = Math.max(maxProfit, profit);
+            minPrice = Math.min(minPrice, prices[i]);
+        }
+
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+
+        int[] prices = {7, 1, 5, 3, 6, 4};
+
+        System.out.println(maxProfit(prices)); // Output: 5
+    }
+}
+
